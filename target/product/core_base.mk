e80a0382a460289527b1ea3b80caae8cd1f15175
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-08-04 13:25:00 +0100

    Enable the SELinux dialog in Settings
    Change-Id: Icc708416d062bc811c83bce0841331c84afcb3f6

--- target/product/core_base.mk
+++ target/product/core_base.mk
@@ -20,6 +20,9 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.notification_sound=Chime.ogg \
     ro.config.alarm_alert=Flow.ogg
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
 PRODUCT_PACKAGES += \
     ContactsProvider \
     DefaultContainerService \
