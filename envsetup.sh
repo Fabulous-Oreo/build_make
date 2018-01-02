3932fe2a5e483f379c91720c3862e58c36c50b0f
Kshitij Gupta <kshitijgm@gmail.com>
2018-01-02 14:56:33 +0530

    envsetup: Check conditions properly on ZSH
    Change-Id: Ia173ae9bf38b8bb74d77e1142fef251b305e5c0b
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -583,7 +583,7 @@ function lunch()
     then
         if [ $answer -le ${#LUNCH_MENU_CHOICES[@]} ]
         then
-            if [ "$(__detect_shell)" == "zsh" ]; then
+            if [[ "$(__detect_shell)" == "zsh" ]]; then
                 selection=${LUNCH_MENU_CHOICES[$(($answer))]}
             else
                 selection=${LUNCH_MENU_CHOICES[$(($answer-1))]}
@@ -842,7 +842,7 @@ function mmm()
         local GET_INSTALL_PATH=
         local GET_INSTALL_PATHS=
 
-        if [ "$(__detect_shell)" == "zsh" ]; then
+        if [[ "$(__detect_shell)" == "zsh" ]]; then
             set -lA DASH_ARGS $(echo "$@" | awk -v RS=" " -v ORS=" " '/^-.*$/')
             set -lA DIRS $(echo "$@" | awk -v RS=" " -v ORS=" " '/^[^-].*$/')
         else
@@ -931,7 +931,7 @@ function mmma()
   local T=$(gettop)
   local DRV=$(getdriver $T)
   if [ "$T" ]; then
-    if [ "$(__detect_shell)" == "zsh" ]; then
+    if [[ "$(__detect_shell)" == "zsh" ]]; then
         set -lA DASH_ARGS $(echo "$@" | awk -v RS=" " -v ORS=" " '/^-.*$/')
         set -lA DIRS $(echo "$@" | awk -v RS=" " -v ORS=" " '/^[^-].*$/')
     else
