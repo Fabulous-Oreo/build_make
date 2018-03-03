02d54aa81c306e399af34e4ff5b55b815a94f5f4
Kshitij Gupta <kshitijgm@gmail.com>
2018-03-03 12:28:41 +0530

    Remove sysinit for now
    Change-Id: Iafc31936a5655ea44e6fd605119089590e6ace2e
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- target/product/core.mk
+++ target/product/core.mk
@@ -61,11 +61,6 @@ PRODUCT_PACKAGES += \
     vr \
     MmsService
 
-# Init.d support
-PRODUCT_PACKAGES += init.d.rc
-PRODUCT_COPY_FILES += \
-    system/core/rootdir/sysinit:vendor/bin/sysinit
-
 # Themes
 PRODUCT_PACKAGES += \
     DeepPurpleTheme \
